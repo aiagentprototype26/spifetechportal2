d75cc9cea149af3cdb9363b97dde263dc4a77ebe
aiagentprototype26 <jamelmurray2014@gmail.com>
2026-07-04 11:27:21 -0400

    Refactor main.jsx to support Apply component

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Dispatcher from "./Dispatcher.jsx";
+import Apply from "./Apply.jsx";
 import "./index.css";
 
-const isDispatcher = window.location.pathname.startsWith("/dispatch");
+const path = window.location.pathname;
+const isDispatcher = path.startsWith("/dispatch");
+const isApply = path.startsWith("/apply");
+
+const Page = isDispatcher ? Dispatcher : isApply ? Apply : App;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>{isDispatcher ? <Dispatcher /> : <App />}</React.StrictMode>
+  <React.StrictMode>
+    <Page />
+  </React.StrictMode>
 );

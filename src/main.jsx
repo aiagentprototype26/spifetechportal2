c226dcf82e9ce341878c92912ebe0b4b95749db7
aiagentprototype26 <jamelmurray2014@gmail.com>
2026-07-20 08:39:12 -0400

    Add CarpetHome component to routing logic

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,16 @@ import App from "./App.jsx";
 import Dispatcher from "./Dispatcher.jsx";
 import Apply from "./Apply.jsx";
 import Client from "./Client.jsx";
+import CarpetHome from "./CarpetHome.jsx";
 import "./index.css";
 
 const path = window.location.pathname;
 const isDispatcher = path.startsWith("/dispatch");
 const isApply = path.startsWith("/apply");
 const isClient = path.startsWith("/book");
+const isCarpet = path.startsWith("/carpet");
 
-const Page = isDispatcher ? Dispatcher : isApply ? Apply : isClient ? Client : App;
+const Page = isDispatcher ? Dispatcher : isApply ? Apply : isClient ? Client : isCarpet ? CarpetHome : App;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
